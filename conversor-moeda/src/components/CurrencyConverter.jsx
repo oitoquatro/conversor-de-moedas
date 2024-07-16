aa945309e0aac895d1f2ea1a6163b5ed1c707f71
Paulo Junior <paulojpjunior@gmail.com>
2024-07-16 20:10:17 -0300

    conversor de moedas
    Aplicando API para conversão de moeda utilizando AXIOS.

--- conversor-moeda/src/components/CurrencyConverter.jsx
+++ conversor-moeda/src/components/CurrencyConverter.jsx
@@ -1,22 +1,76 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./CurrencyConverter.css";
 
 const CurrencyConverter = () => {
+  const [rates, setRates] = useState(null);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://v6.exchangerate-api.com/v6/348f1a00d0d92278c12411af/latest/USD"
+      )
+      .then((response) => {
+        setRates(response.data.conversion_rates);
+      })
+      .catch((error) => {
+        console.log("Erro ao obter dados da API", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (rates) {
+      const rateFrom = rates[fromCurrency] || 0;
+      const rateTo = rates[toCurrency] || 0;
+      setConvertedAmount(((amount / rateFrom) * rateTo).toFixed(2));
+    }
+  }, [amount, rates, fromCurrency, toCurrency]);
+
+  if (!rates) {
+    return <div>Carregando...</div>;
+  }
+
   return (
     <div className="converter">
       <h2>Conversor de Moedas</h2>
-      <input type="number" placeholder="Digite o valor..."/>
-      <span>Selecione as moedas</span>
-      <select>
-        <option value="BRL">BRL</option>
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <span>Selecione as moedas:</span>
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
+        {Object.keys(rates).map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
       </select>
-      <span>para</span>
-      <select>
-        <option value="USD">USD</option>
+      <span> para </span>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
+        {Object.keys(rates).map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
       </select>
-      <h3>BRL USD</h3>
-      <p>20 BRL valem 5 USD</p>
+      <h3>
+        {convertedAmount} {toCurrency}
+      </h3>
+      <p>
+        {amount} {fromCurrency} valem {convertedAmount} {toCurrency}
+      </p>
     </div>
   );
 };
